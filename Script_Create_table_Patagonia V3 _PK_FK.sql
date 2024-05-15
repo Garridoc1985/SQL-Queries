ff2857ae60ffec093c3f1566e17ebefbb2aca034
Sebastián Garrido Cood <137034165+Garridoc1985@users.noreply.github.com>
2024-05-14 23:17:43 -0400

    Update Script_Create_table_Patagonia V3 _PK_FK.sql

--- Script_Create_table_Patagonia V3 _PK_FK.sql
+++ Script_Create_table_Patagonia V3 _PK_FK.sql
@@ -3,28 +3,6 @@ CREATE DATABASE IF NOT EXISTS Patagonia;
 
 USE Patagonia;
 
-
-CREATE TABLE ventas (
-	id_venta INT AUTO_INCREMENT PRIMARY KEY,
-    id_documento INT,
-    id_sucursal INT,
-    id_estado_documento INT,
-    id_vendedor INT,
-    fecha_venta DATE,
-    total_venta INT,
-    FOREIGN KEY (id_documento) REFERENCES documentos(id_documento),
-    FOREIGN KEY (id_sucursal) REFERENCES sucursales(id_sucursal),
-    FOREIGN KEY (id_estado_documento) REFERENCES estado_documentos(id_estado_documento),
-    FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor)
-);
-
-CREATE TABLE sucursales (
-    id_sucursal INT AUTO_INCREMENT PRIMARY KEY,
-    id_ubicacion_sucursal INT,
-	nombre_sucursal VARCHAR(55),
-    FOREIGN KEY (id_ubicacion_sucursal) REFERENCES ubicacion_sucursales(id_ubicacion_sucursal)
-);
-
 CREATE TABLE ubicacion_sucursales (
     id_ubicacion_sucursal INT AUTO_INCREMENT PRIMARY KEY,
     region VARCHAR(55),
@@ -38,13 +16,7 @@ CREATE TABLE vendedores (
     nombre_vendedor VARCHAR(55)
 );
 
-CREATE TABLE reclamos (
-    id_reclamos INT AUTO_INCREMENT PRIMARY KEY,
-    fecha_reclamos DATE,
-    id_vendedor INT,
-    descripcion_reclamo TEXT,
-    FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor)
-);
+
 
 CREATE TABLE documentos (
     id_documento INT AUTO_INCREMENT PRIMARY KEY,
@@ -56,12 +28,7 @@ CREATE TABLE estado_documentos (
     estado_documento VARCHAR(55)
 );
 
-CREATE TABLE productos_ventas (
-    id_producto_venta INT AUTO_INCREMENT PRIMARY KEY,
-    id_producto INT,
-    tipo_venta VARCHAR(55),
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
-);
+
 
 CREATE TABLE productos (
     id_producto INT AUTO_INCREMENT PRIMARY KEY,
@@ -70,17 +37,7 @@ CREATE TABLE productos (
     precio_producto DECIMAL(10, 2)
 );
 
-CREATE TABLE stock (
-    id_stock INT AUTO_INCREMENT PRIMARY KEY,
-    id_proveedor INT,
-    id_producto INT,
-    id_promocion INT,
-    fecha_stock DATE,
-    cantidad_stock INT,
-    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor),
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
-    FOREIGN KEY (id_promocion) REFERENCES promociones(id_promocion)
-);
+
 
 CREATE TABLE promociones (
     id_promocion INT AUTO_INCREMENT PRIMARY KEY,
@@ -97,10 +54,60 @@ CREATE TABLE proveedores (
     fecha_compra_proveedor DATE 
 );
 
+
 CREATE TABLE documento_proveedor (
     id_documento_proveedor INT AUTO_INCREMENT PRIMARY KEY,
     id_proveedor INT,
     fecha_emision DATE,
     total_factura INT,
     FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
+);
+
+
+CREATE TABLE sucursales (
+    id_sucursal INT AUTO_INCREMENT PRIMARY KEY,
+    id_ubicacion_sucursal INT,
+	nombre_sucursal VARCHAR(55),
+    FOREIGN KEY (id_ubicacion_sucursal) REFERENCES ubicacion_sucursales(id_ubicacion_sucursal)
+);
+
+CREATE TABLE reclamos (
+    id_reclamos INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_reclamos DATE,
+    id_vendedor INT,
+    descripcion_reclamo TEXT,
+    FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor)
+);
+
+CREATE TABLE productos_ventas (
+    id_producto_venta INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT,
+    tipo_venta VARCHAR(55),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+);
+
+CREATE TABLE stock (
+    id_stock INT AUTO_INCREMENT PRIMARY KEY,
+    id_proveedor INT,
+    id_producto INT,
+    id_promocion INT,
+    fecha_stock DATE,
+    cantidad_stock INT,
+    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+    FOREIGN KEY (id_promocion) REFERENCES promociones(id_promocion)
+);
+
+CREATE TABLE ventas (
+	id_venta INT AUTO_INCREMENT PRIMARY KEY,
+    id_documento INT,
+    id_sucursal INT,
+    id_estado_documento INT,
+    id_vendedor INT,
+    fecha_venta DATE,
+    total_venta INT,
+    FOREIGN KEY (id_documento) REFERENCES documentos(id_documento),
+    FOREIGN KEY (id_sucursal) REFERENCES sucursales(id_sucursal),
+    FOREIGN KEY (id_estado_documento) REFERENCES estado_documentos(id_estado_documento),
+    FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor)
 );

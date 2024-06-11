52dcacf392f5348dc6bd7b15644254846e520380
Sebastián Garrido Cood <137034165+Garridoc1985@users.noreply.github.com>
2024-06-10 20:21:28 -0400

    Update Script_stored_procedures_Table_Patagonia VF.sql

--- Script_stored_procedures_Table_Patagonia VF.sql
+++ Script_stored_procedures_Table_Patagonia VF.sql
@@ -1,33 +1,33 @@
-DELIMITER $$
+DELIMITER //
 
 CREATE PROCEDURE eliminar_documento_proveedor(IN id_documento INT)
 BEGIN
     DELETE FROM documento_proveedor
     WHERE id_documento_proveedor = id_documento;
-END$$
+END//
 
 DELIMITER ;
 
-DELIMITER $$
+DELIMITER //
 
 CREATE PROCEDURE actualizar_proveedor(IN id_proveedor_parametro INT, IN nuevo_nombre VARCHAR(100))
 BEGIN
     UPDATE proveedores
     SET nombre_proveedor = nuevo_nombre
     WHERE id_proveedor = id_proveedor_parametro;
-END$$
+END//
 
 DELIMITER ;
 
 
-DELIMITER $$
+DELIMITER //
 
 CREATE PROCEDURE obtener_facturas_por_fecha(IN fecha_inicio DATE, IN fecha_fin DATE)
 BEGIN
     SELECT * 
     FROM documento_proveedor
     WHERE fecha_emision BETWEEN fecha_inicio AND fecha_fin;
-END$$
+END//
 
 DELIMITER ;
 
